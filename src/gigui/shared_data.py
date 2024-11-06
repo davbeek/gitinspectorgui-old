85126825f8593bba1ed2c22c190be94c59f6f221
Bert van Beek <d.a.v.beek@tue.nl>
2024-11-06 19:49:41 +0100

    Revert to correct type hint for repo var in shared_data module

--- src/gigui/shared_data.py
+++ src/gigui/shared_data.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg  # type: ignore
 
+from gigui.repo import GIRepo
 from gigui.typedefs import Html
 
 # Set to True at the start of gui.psg.run
@@ -8,8 +9,7 @@ gui = False  # pylint: disable=invalid-name
 # Set to the value of the GUI window at the start of gui.psg.run
 gui_window: sg.Window | None = None
 
-# Cannot import type hint GIRepo for current_repo due to circular import
-current_repo = None  # pylint: disable=invalid-name
+current_repo: GIRepo  # pylint: disable=invalid-name
 repo_name: str = ""
 html_code: Html = ""
 css_code: str = ""
