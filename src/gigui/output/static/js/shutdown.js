55f162d3138b31306e8e901a6f074a2d9f560a9a
Bert van Beek <d.a.v.beek@tue.nl>
2024-11-14 11:59:51 +0100

    Simplify js shutdown script

--- src/gigui/output/static/js/shutdown.js
+++ src/gigui/output/static/js/shutdown.js
@@ -3,13 +3,6 @@ window.addEventListener('beforeunload', function (event) {
     const browserId = '<%= browser_id %>'; // This will be replaced with the actual browser ID
     const url = `http://localhost:${port}/shutdown?id=${browserId}`; // Include the browser ID in the URL
 
-    if (navigator.sendBeacon) {
-        const data = new Blob([], { type: 'application/x-www-form-urlencoded' });
-        navigator.sendBeacon(url, data);
-    } else {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url, false);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send();
-    }
+    const data = new Blob([], { type: 'application/x-www-form-urlencoded' });
+    navigator.sendBeacon(url, data);
 });
